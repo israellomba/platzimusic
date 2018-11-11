357a8dccc8e75949bea10fd2e74a45cd7f9a0e6b
Israel Lombardero <israellomba@gmail.com>
2018-11-10 22:36:15 -0600

    master

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,6 @@
 <template lang="pug">
   #app
-    img(src='./assets/logo.png')
+    img(src='dist/logo.png')
     h1 Platzimusic
     select(v-model="selectedCountry")
       option(v-for="country in countries" v.bind:value="country.value") {{country.name}}
@@ -66,10 +66,20 @@ export default {
 h1, h2 
 	font-weight normal
 ul 
+  display block
 	list-style-type none
+  margin-block-start 1em
+  margin-block-end 1em
+  margin-inline-start 0px
+  margin-inline-end 0px
+  padding-inline-start 40px
 	padding 0
 li 
-	display inline-block
+  display inline-block
+  padding 5px
+  margin 10px
+  border 2px solid #41b883
+  border-radius 5px
 	margin 0 10px
 a 
 	color #42b983
